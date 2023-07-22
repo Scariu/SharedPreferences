bdf0bc48995f6477e619b6972749836c289b9fe6
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-21 20:52:40 -0400

    Se implemeta buttonGuardar y sharedPreferences.

--- app/src/main/java/com/example/project_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/project_sharedpreferences/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.project_sharedpreferences
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.project_sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mSharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,11 +17,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
+        mSharedPreferences = getSharedPreferences("cookie", Context.MODE_PRIVATE)
         binding.buttonGuardar.setOnClickListener{
             val entero = binding.editTextNumber.text.toString().toInt()
             val texto = binding.editTextTexto.text.toString()
-            val decimal = binding.editTextNumberDecimal.text.toString().toInt()
-            val switch = binding.switchHello.isChecked
+            val decimal = binding.editTextNumberDecimal.text.toString().toFloat()
+            val switchBoleano = binding.switchHello.isChecked
+
+            guardarDatos(texto, entero, decimal, switchBoleano)
         }
+    }
+
+    //Guarda datos en sharedPreferences
+    private fun guardarDatos(texto: String, entero: Int, decimal: Float, switchBoleano: Boolean) {
+        mSharedPreferences.edit().putInt("miEntero", entero).apply()
+        mSharedPreferences.edit().putString("miTexto", texto).apply()
+        mSharedPreferences.edit().putFloat("miDecimal", decimal).apply()
+        mSharedPreferences.edit().putBoolean("miSwitch", switchBoleano).apply()
     }
 }
