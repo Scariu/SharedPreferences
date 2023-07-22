99c5277854e040fb8e9abf3046c0d781f08bfaa7
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-21 20:33:08 -0400

    Se inicializa binding en MainActivity.

--- app/src/main/java/com/example/project_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/project_sharedpreferences/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.project_sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
